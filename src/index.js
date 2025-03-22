f8265461e25c61be82e8b700bdeb37ec6fc8315b
Sasikumar <santhamsasi68670@gmail.com>
2025-03-22 11:32:36 +0530

    Adding Image Optimization

--- src/index.js
+++ src/index.js
@@ -11,8 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      {/* <App/> */}
-      <Home/>
+      <App/>
     </React.StrictMode>
   </BrowserRouter>
 );
